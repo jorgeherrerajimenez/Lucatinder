53ad6f3de52cb740f368b9cd94fbad7ea6af0308
jorgeherrerajimenez <60509316+jorgeherrerajimenez@users.noreply.github.com>
2020-06-02 11:43:39 +0200

    Primer commit

---
+++ src/main/java/lucatic/grupo1/LucatinderApplication.java
@@ -0,0 +1,13 @@
+package lucatic.grupo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucatinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucatinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lucatic/grupo1/LucatinderApplicationTests.java
@@ -0,0 +1,13 @@
+package lucatic.grupo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucatinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
