3de961a27c81f176db0209037f8cdee76b9990c9
Marco Rueda <marcorueda92z@gmail.com>
2020-06-02 23:00:52 +0200

    TRABAJANDO EN PRUEBAS UNITARIAS

--- src/main/java/lucatic/grupo1/repository/DAOPerfil.java
+++ src/main/java/lucatic/grupo1/repository/DAOPerfil.java
@@ -2,6 +2,8 @@ package lucatic.grupo1.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import lucatic.grupo1.model.Perfil;
+
 public interface DAOPerfil extends JpaRepository<Perfil, Integer>{
 	
 
