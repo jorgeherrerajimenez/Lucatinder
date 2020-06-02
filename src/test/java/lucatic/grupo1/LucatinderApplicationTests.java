4fe10354e1d6ac85d17e3a6b4410aaba1a790959
Marco Rueda <marcorueda92z@gmail.com>
2020-06-02 18:36:04 +0200

    commit para update master

--- src/test/java/lucatic/grupo1/LucatinderApplicationTests.java
+++ src/test/java/lucatic/grupo1/LucatinderApplicationTests.java
@@ -1,13 +1,20 @@
 package lucatic.grupo1;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class LucatinderApplicationTests {
 
 	@Test
 	void contextLoads() {
+		
+		assertThat(false).isTrue();
 	}
 
 }
