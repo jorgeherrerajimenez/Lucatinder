79366cbc406d1469830e063aa0486a0fa4c3e2db
Jorge Herrera Jimenez <jorgehj2398@gmail.com>
2020-06-02 18:28:53 +0200

    Materia, perfil and DAOMateria

---
+++ src/main/java/lucatic/grupo1/model/Materia.java
@@ -0,0 +1,47 @@
+package lucatic.grupo1.model;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Materia {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	private String nombre;
+	
+	@ManyToMany(mappedBy = "gustosInformaticos")
+	private List<Perfil> perfiles;
+	
+	
+	public Materia() {
+		super();
+	}
+	public Materia(String nombre) {
+		super();
+		this.nombre = nombre;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	@Override
+	public String toString() {
+		return "Materia [id=" + id + ", nombre=" + nombre + "]";
+	}
+}
+
---
+++ src/main/java/lucatic/grupo1/model/Perfil.java
@@ -0,0 +1,102 @@
+package lucatic.grupo1.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.JoinColumn;
+
+@Entity
+public class Perfil implements Serializable {
+
+	private static final long serialVersionUID = 6529425024800979551L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	private String nombre;
+	private char genero;
+	private short edad;
+	private String descripcion;
+	
+	@ManyToMany
+	@JoinTable(
+			  name = "materia_perfil", 
+			  joinColumns = @JoinColumn(name = "materia_id"), 
+			  inverseJoinColumns = @JoinColumn(name = "perfil_id"))
+	private List<Materia> gustosInformaticos;
+	
+	/*private List<Descarte> descartados;
+	
+	private List<Match> matchs;
+	
+	private List<Contacto> contactos;*/
+	
+	
+
+	public Perfil() {
+		super();
+	}
+
+	public Perfil(Long id, String nombre, char genero, short edad, String descripcion,
+			List<Materia> gustosInformaticos) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.genero = genero;
+		this.edad = edad;
+		this.descripcion = descripcion;
+		this.gustosInformaticos = gustosInformaticos;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public char getGenero() {
+		return genero;
+	}
+
+	public void setGenero(char genero) {
+		this.genero = genero;
+	}
+
+	public short getEdad() {
+		return edad;
+	}
+
+	public void setEdad(short edad) {
+		this.edad = edad;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public List<Materia> getGustosInformaticos() {
+		return gustosInformaticos;
+	}
+	
+}
