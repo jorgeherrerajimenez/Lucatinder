004d582abfbb4738a4ba25e406eb1e10a5bab581
Maira Peña Quiroz <maira.pquiroz@gmail.com>
2020-06-12 12:16:31 +0200

    Actualizar Rama Maira

--- src/main/java/lucatic/grupo1/controller/PerfilRESTController.java
+++ src/main/java/lucatic/grupo1/controller/PerfilRESTController.java
@@ -3,6 +3,7 @@ package lucatic.grupo1.controller;
 import java.util.List;
 import java.util.logging.Logger;
 import java.util.logging.Level;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.http.HttpStatus;
+
 import lucatic.grupo1.model.Perfil;
 import lucatic.grupo1.model.rs.PerfilResponse;
 import lucatic.grupo1.service.ContactoService;
